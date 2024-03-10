6d3bab98d24a84671506945198aaebad08b7e3ce
Nadeem Ashraf <dev.nad.ash@gmail.com>
2024-03-11 00:51:27 +0530

    Remove dist directory

--- dist/routes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = void 0;
-var rooms_1 = require("./rooms");
-Object.defineProperty(exports, "default", { enumerable: true, get: function () { return __importDefault(rooms_1).default; } });
